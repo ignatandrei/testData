028c9063bdab8fefa7c140f65b9494dd4fb73e95
Andrei Ignat <ignatandrei@yahoo.com>
2020-09-27 05:07:08 +0300

    latest

---
+++ src/saveBox/src/app/classes/scan.ts
@@ -0,0 +1,13 @@
+enum Types {
+    Type1 = 1,
+    Type2,
+    Type3,
+    Type4
+  }
+
+export class ScanData{
+    
+    public platz: string;
+    public box: string;
+    public typeScan: Types;
+}
